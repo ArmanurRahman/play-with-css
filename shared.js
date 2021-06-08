6f278ac9a094dff15c4a2beae018dac009ed3a70
mubeen <mubeenarman19@gmail.com>
2021-06-08 20:51:14 +0900

    add mobile nav bar

--- shared.js
+++ shared.js
@@ -2,20 +2,44 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var selectedPlanButtons = document.querySelectorAll(".plan button");
 var modalCancelButton = document.querySelector(".modal button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNavbar = document.querySelector(".mobile-nav");
 
 selectedPlanButtons.forEach((item) => {
     item.addEventListener("click", () => {
-        backdrop.style.display = "block";
-        modal.style.display = "block";
+        openModal();
     });
 });
 
-modalCancelButton.addEventListener("click", () => {
-    backdrop.style.display = "none";
-    modal.style.display = "none";
-});
+if (modalCancelButton) {
+    modalCancelButton.addEventListener("click", () => {
+        closeModal();
+    });
+}
 
 backdrop.addEventListener("click", () => {
-    backdrop.style.display = "none";
-    modal.style.display = "none";
+    closeModal();
+});
+
+const closeModal = () => {
+    //backdrop.style.display = "none";
+    //modal.style.display = "none";
+    backdrop.classList.remove("open");
+    if (modal) {
+        modal.classList.remove("open");
+    }
+
+    mobileNavbar.classList.remove("open");
+};
+
+const openModal = () => {
+    //backdrop.style.display = "block";
+    //modal.style.display = "block";
+    backdrop.classList.add("open");
+    modal.classList.add("open");
+};
+
+toggleButton.addEventListener("click", () => {
+    backdrop.classList.add("open");
+    mobileNavbar.classList.add("open");
 });
